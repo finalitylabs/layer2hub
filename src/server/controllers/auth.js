cd252735f237676584fc787bac26356a4a665d32
Lex Lewthor <alexvlewis@gmail.com>
2018-07-12 08:54:58 -0700

    Properly disable firebase jwt auth example

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -3,15 +3,16 @@ const response = require('../helpers/response')
 
 // swagger-tools auth controller
 module.exports.jwt = (req, def, token, next) => {
-  if (!token) {
-    response.error(req, 403, 'Forbidden', next)
-  } else {
-    req.data.authUser = { id: 'jwt_disabled' }
-    // firebase.auth().verifyIdToken(token).then((user) => {
-    //   req.data.authUser = user
-    //   next()
-    // }).catch((error) => {
-    //   response.error(req, 401, 'Unauthorized', next)
-    // })
-  }
+  req.data.authUser = { id: 'jwt_disabled' }
+  next()
+  // if (!token) {
+  //   response.error(req, 403, 'Forbidden', next)
+  // } else {
+  //   firebase.auth().verifyIdToken(token).then((user) => {
+  //     req.data.authUser = user
+  //     next()
+  //   }).catch((error) => {
+  //     response.error(req, 401, 'Unauthorized', next)
+  //   })
+  // }
 }
